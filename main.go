f8a7e47ec5ceb1c387fb74615921d2f4a0ed03aa
Dijah800 <68284838+Dijah800@users.noreply.github.com>
2020-08-13 20:59:07 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+func home(w http.ResponseWriter, r *http.Request) {
+ msg := "Hello, welcome to your app. Use the following suffix's on the URL to show the different results.\n1)'/scrape' to show results of web scraping.\n2)'/crawl' to show results of a web crawler"
+ logr.Info("Received request for the home page")
+  w.Write([]byte(fmt.Sprintf(msg)))
+}
+import (
+     "fmt"
+     "net/http"
+     logr "github.com/sirupsen/logrus""net/http"
+)
+http.HandleFunc("/", home)
+logr.Info("Starting up on 8080")
+http.ListenAndServe(":8080", nil)
