50d12116c58228f0a4d3e49e6ec1921f060fa1b7
Dijah800 <68284838+Dijah800@users.noreply.github.com>
2020-08-15 21:11:38 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+func home(w http.ResponseWriter, r *http.Request) {
+  mssg := "Hello, welcome to your app. Use the following suffix's on the URL to show the different results./n1)'/scrape' to show
+  logr.Info("Received request for the home page")
+  w.Write([]byte(fmt.Sprintf(msg)))
+  import (
+    "fmt"
+    "net/http
+     logr "github.com/sirupsen/logrus"
+  )
